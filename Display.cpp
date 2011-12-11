a94674361fa72e2ab2159c877a9d2d70717e6f2d
Jim Zhang <Hyriodula@gmail.com>
2011-12-11 11:47:11 -0500

    Un-screwing up Part 1

--- Display.cpp
+++ Display.cpp
@@ -3,14 +3,70 @@
 /* Changes a single pixel located at (x, y) to RGB values (r, g, b) */
 void Display::putpixel(int x, int y, uint8_t r, uint8_t g, uint8_t b)
 {
-    if(SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
-    *((Uint32*)screen->pixels+screen->w*y + x) = SDL_MapRGB(screen->format, r, g, b);
-    if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+    if(SDL_MUSTLOCK(gScreen)) SDL_LockSurface(gScreen);
+    *((Uint32*)gScreen->pixels+gScreen->w*y + x) = SDL_MapRGB(gScreen->format, r, g, b);
+    if(SDL_MUSTLOCK(gScreen)) SDL_UnlockSurface(gScreen);
     return;
 }
 
-/* Initiates graphics */
+/* Updates graphical changes */
+void Display::update()
+{
+    SDL_Flip(gScreen);
+    return;
+}
+
+/* Initialize graphics */
 Display::Display(int width, int height)
 {
-    screen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE);
+    // Initialize SDL's subsystems... This code will eventually be moved to display
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    //SDL_WM_SetIcon(SDL_LoadBMP(LOCATION OF THE ICON BITMAP), NULL);
+    SDL_WM_SetCaption("Morphling","Morphling");
+
+    if (TTF_Init() == -1)
+    {
+        printf("Unable to initialize SDL_ttf: %s \n", TTF_GetError());
+        exit(2);
+    }
+
+
+
+    /* Get available fullscreen/hardware modes */
+    SDL_Rect ** modes=SDL_ListModes(NULL, SDL_FULLSCREEN|SDL_HWSURFACE);
+    /* Check is there are any modes available */
+    if(modes == (SDL_Rect **)0)
+    {
+        exit(-1);
+    }
+    /* Check if or resolution is restricted */
+    if(modes == (SDL_Rect **)-1)
+    {
+        exit(-2);
+    }
+    else
+    {
+        /* Do first mode */
+        gScreen = SDL_SetVideoMode(width,height, 32, SDL_SWSURFACE);
+    }
+
+
+    // currently is #defined width and height, a simple change (see below) and a fullscreen toggle will allow the screen to be custom sized. may cause bugs, until ready use #defs.
+    //gScreen = SDL_SetVideoMode(0, 0, 32, SDL_SWSURFACE);
+
+    // If we fail, return error.
+    if (gScreen == NULL)
+    {
+        fprintf(stderr, "Unable to set up video: %s\n", SDL_GetError());
+        exit(-3);
+    }
+
+    // clear screen just in case
+    SDL_FillRect(gScreen,NULL,0x000000);
+    gScreen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE);
 }
--- Display.h
+++ Display.h
@@ -1,12 +1,6 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-
-/*#ifdef _WIN32
-#include <SDL.h>
-#else
-#include <SDL/SDL.h>
-#endif*/
 #if defined(_MSC_VER)
 #include "SDL.h"
 #include "SDL_ttf.h"
@@ -16,9 +10,10 @@
 #endif
 
 class Display{
-	SDL_Surface *screen;
+	SDL_Surface *gScreen;
 	public:
 	void putpixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+	void update();
 	Display(int width, int height);
 };
 
