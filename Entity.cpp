fbcc82ff7f8662e78305317060cbdeb27b71111c
Tony Hu <hu.tinghan@gmail.com>
2011-12-19 22:21:30 -0500

    Started entity files

---
+++ Entity.cpp
@@ -0,0 +1,6 @@
+#include "Entity.h"
+
+int main()
+{
+	return 0;
+}
---
+++ Entity.h
@@ -0,0 +1,43 @@
+#ifndef ENTITY_H // Guard word
+#define ENTITY_H
+
+#include <iostream>
+#include <string>
+#include <list>
+
+class Entity
+{
+protected:
+	std::string n; // Entity's name
+	std::string t; // Type of entity
+	int maxhp; //
+	int hp;    //
+	int maxm;  // Basic attributes
+	int m;     // of the entity
+	int str;   //
+	int amr;   //
+	std::list<Ability> a; // Entity's abilities
+public:
+	Entity();                      // Constructor
+	std::string& name();           //
+	std::string& type();           //
+	int& maxHitpoints();           //
+	int& hitpoints();              //
+	int& maxMana();                // References to
+	int& mana();                   // protected members
+	int& strength();               //
+	int& armor();                  //
+	std::list<Ability>& abilities; //
+};
+
+class Player : public Entity
+{
+	std::list<Items> i; // Inventory
+	int gp;             // Gold pieces
+public:
+	Player();                  // Constructor
+	std::list<Items>& items(); // References to
+	int& gold();               // private members
+};
+
+#endif
