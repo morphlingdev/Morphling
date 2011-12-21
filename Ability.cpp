a127cd241fb0df3e6aab05fc7d33149b1156e8fc
Charles Zinn <charles.zinn@gmail.com>
2011-12-20 21:11:37 -0500

    Changed some stuff in the Ability class.

--- Ability.cpp
+++ Ability.cpp
@@ -9,6 +9,14 @@ using namespace std;
 Ability::Ability(string filename)
 {
     //Set default values
+    name = "Default";
+    desc = "None.";
+    target_type = TARGET_LINE;
+    range = 1;
+    aoe_size = 0;
+    aoe_shape = SHAPE_SQUARE;
+    channel_time = 0;
+    cooldown = 0;
 
     //Open the file
     ifstream afile;
@@ -19,9 +27,100 @@ Ability::Ability(string filename)
         while(afile.good()) //Loop until eof
         {
             getline(afile, line);
-            //Parse the line (for now output for testing)
-            cout << line << endl;
-        }
+            if(line.compare("") != 0)
+            {
+                //Parse the line
+                int colon = line.find(":"); //position of first :
+                string command = line.substr(0, colon);
+                string vals = line.substr(colon + 1, line.length());
+                if(command.compare("name") == 0)
+                {
+                    name = vals;
+                }
+                else if(command.compare("desc") == 0)
+                {
+                    desc = vals;
+                }
+                else if(command.compare("passive") == 0)
+                {
+                    colon = vals.find(":");
+                    string s_pstat = vals.substr(0, colon);
+                    string s_pbonus = vals.substr(colon + 1, vals.length());
+                    int pstat;
+                    if(s_pstat.compare("str") == 0)
+                    {
+                        pstat = STAT_STR;
+                    }
+                    else if(s_pstat.compare("int") == 0)
+                    {
+                        pstat = STAT_INT;
+                    }
+                    else if(s_pstat.compare("bob") == 0)
+                    {
+                        pstat = STAT_BOB;
+                    }
+                    int pbonus = atoi(s_pbonus.c_str());
 
+                    passive[pstat] += pbonus;
+                }
+                else if(command.compare("target") == 0)
+                {
+                    //TYPE_LINE is default, so we only need to check for "smite"
+                    if(vals.compare("smite") == 0)
+                    {
+                        target_type = TYPE_SMITE;
+                    }
+                }
+                else if(command.compare("range") == 0)
+                {
+                    range = atoi(vals.c_str());
+                }
+                else if(command.compare("channel") == 0)
+                {
+                    channel = atoi(vals.c_str());
+                }
+                else if(command.compare("aoe") == 0)
+                {
+                    aoe_size = atoi(vals.c_str());
+                }
+                else if(command.compare("aoeshape") == 0)
+                {
+                    //SHAPE_SQUARE is default, so we only need to check for "circle"
+                    if(vals.compare("circle"))
+                    {
+                        aoe_shape = SHAPE_CIRCLE;
+                    }
+                }
+                else if(command.compare("cost") == 0)
+                {
+                    colon = vals.find(":");
+                    string s_ccost = vals.substr(0, colon);
+                    string s_camt = vals.substr(colon + 1, vals.length());
+                    int ccost;
+                    if(s_pstat.compare("mana") == 0)
+                    {
+                        ccost = COST_MANA;
+                    }
+                    else if(s_pstat.compare("health") == 0)
+                    {
+                        ccost = COST_HEALTH;
+                    }
+                    else if(s_pstat.compare("gold") == 0)
+                    {
+                        ccost = COST_GOLD;
+                    }
+                    int cbonus = atoi(s_pbonus.c_str());
+
+                    cost[ccost] += cbonus;
+                }
+
+
+                cout << line << "(" << command << ")" << endl;
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
--- Ability.h
+++ Ability.h
@@ -4,31 +4,37 @@
 #include <string>
 
 //Constants
-const int TARGET_LINE = 1;
-const int TARGET_SMITE = 2;
-const int STAT_STR = 1;
-const int STAT_INT = 2;
-const int STAT_BOB = 3; //these are examples
-const int COST_MANA = 1;
-const int COST_HEALTH = 2;
-const int COST_GOLD = 3;
+const int TARGET_LINE = 0;
+const int TARGET_SMITE = 1;
+const int SHAPE_SQUARE = 0;
+const int SHAPE_CIRCLE = 1;
+const int STAT_STR = 0;
+const int STAT_INT = 1;
+const int STAT_BOB = 2; //these are examples
+const int COST_MANA = 0;
+const int COST_HEALTH = 1;
+const int COST_GOLD = 2;
+
+const int STAT_NUMBER = 3; //how many types of stats there are
+const int COST_NUMBER = 3; //how many types of costs there are
 
 class Ability
 {
 private:
-    std::string name;
-    std::string desc;
-    int target_type;
-    int range;
-    int aoe_size;
-    int channel_time;
-    int cooldown;
-    int passive[];
-    int costs[];
+std::string name;
+std::string desc;
+int target_type;
+int range;
+int aoe_size;
+int aoe_shape;
+int channel_time;
+int cooldown;
+int passive[STAT_NUMBER];
+int costs[COST_NUMBER];
 
 public:
 
-    Ability(std::string);
+Ability(std::string);
 };
 
 #endif
