cf0ed4dc6e8e75aa81bcc7e1df748f76075052d0
dr-hu <hu.tinghan@gmail.com>
2012-01-04 22:00:40 -0500

    More formatting and some suggestions.

--- Entity.cpp
+++ Entity.cpp
@@ -1,34 +1,35 @@
 #include "Entity.h"
 
+// The following functions will perform basic actions for an entity
 Entity::Entity() // Constructor for any entity
 {
+    // Randomly generate creature type
+    //hp = maxhp = ;
+    //m = maxm = ;
+    //
     // idk if we need this
 }
-
 Entity::~Entity() // Destructor for mobs
 {
     // Drop loot
     // Player gains xp
 }
-
 Player::Player() // Constructor specifically for player
 {
     // idk if we need this
 }
-
 void Player::death() // Goto here when player dies
 {
     hp = maxhp/2; // Attributes when
     m = maxm;     // respawning
     gp /= 2;      //
-    // Need spawning mechanism
+    // Need spawning mechanism (location)
     return;
 }
-
 void Player::level_up()
 {
-    maxhp *= 1.2;
-    hp = maxhp;
+    maxhp *= 1.2; // Assuming 20% increase per level
+    hp = maxhp; // Health regeneration
     maxm *= 1.2;
     m = maxm;
     // experience stuff
@@ -37,50 +38,54 @@ void Player::level_up()
     return;
 }
 
+// The following functions are defined to access private/protected members
 std::string& Entity::name()
 {
-    return n;    //
+    return n;
 }
 std::string& Entity::type()
 {
-    return t;    //
+    return t;
 }
 int& Entity::maxHitpoints()
 {
-    return maxhp;    //
+    return maxhp;
 }
 int& Entity::hitpoints()
 {
-    return hp;    //
+    return hp;
 }
 int& Entity::maxMana()
 {
-    return maxm;    //
+    return maxm;
 }
 int& Entity::mana()
 {
-    return m;    // Class functions defined
+    return m;
 }
 int& Entity::strength()
 {
-    return str;    // to access private/protected
+    return str;
 }
 int& Entity::armor()
 {
-    return amr;    // members
+    return amr;
 }
 int& Entity::level()
 {
-    return lvl;    //
+    return lvl;
 }
 std::list<Ability>& Entity::abilities()
 {
-    return a;    //
+    return a;
 }
-//std::list<Item>& Player::items() {return i;}        //
+//std::list<Item>& Player::items()
+//{
+//  return i;
+//}
 int& Player::gold()
 {
-    return gp;    //
+    return gp;
 }
 int& Player::experience()
 {
