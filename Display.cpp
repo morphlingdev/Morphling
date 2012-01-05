b23533c6938e6fa065141cb67b4f3ea98c417454
Jim Zhang <Hyriodula@gmail.com>
2012-01-05 15:50:36 -0500

    Made Display comments more organized and pretty.

--- Display.cpp
+++ Display.cpp
@@ -1,18 +1,29 @@
 #include "Display.h"
 
-/* Changes a single pixel located at (x, y) to RGB values (r, g, b) */
+/** The wondrous Display class.
+  *
+  * It handles /everything/ graphical.
+  * You want to draw something onto the screen?
+  * Call one of Display's methods and it handles everything. Fun!
+**/
+
+/*
+ * Geometrical primitives and technical functions
+ */
+
+/* Makes any changes visible */
+void Display::update()
+{
+    SDL_Flip(gScreen);
+    return;
+}
+
+/* Changes a single pixel, located at (x, y), to (r, g, b) */
 void Display::putpixel(int x, int y, uint8_t r, uint8_t g, uint8_t b)
 {
     if(SDL_MUSTLOCK(gScreen)) SDL_LockSurface(gScreen);
     *((Uint32*)gScreen->pixels+gScreen->w*y + x) = SDL_MapRGB(gScreen->format, r, g, b);
     if(SDL_MUSTLOCK(gScreen)) SDL_UnlockSurface(gScreen);
-    return;
-}
-
-/* Make graphical changes visible */
-void Display::update()
-{
-    SDL_Flip(gScreen);
     return;
 }
 
@@ -28,7 +39,51 @@ void Display::fill_rect(int x, int y, int w, int h, int r, int g, int b)
     return;
 }
 
-/* Draw tile such that its upper-left pixel is at (x, y) */
+/*
+ * Text
+ */
+
+/* Draws a string on a single line */
+void Display::draw_text_line(SDL_Rect *dst, std::string txt, FontType type, SDL_Color col)
+{
+    SDL_Surface *render = TTF_RenderText_Solid(font[type], txt.c_str(), col);
+    SDL_BlitSurface(render, NULL, gScreen, dst);
+    SDL_FreeSurface(render);
+    return;
+}
+void Display::draw_text_line(int x, int y, std::string txt, FontType type, int r, int g, int b)
+{
+    SDL_Color col;
+    SDL_Rect dst;
+
+    col.r = r;
+    col.g = g;
+    col.b = b;
+    dst.x = x;
+    dst.y = y;
+
+    draw_text_line(&dst, txt, type, col);
+}
+
+/* Draws text to fit inside a rectangle, with REALLY basic character wrapping */
+void Display::draw_text_block(int x, int y, int w, std::string txt, FontType type, int r, int g, int b)
+{
+    int char_width, line_height, chars_per_line, num_lines;
+    TTF_SizeText(font[type], "a", &char_width, NULL);
+    line_height = TTF_FontLineSkip(font[type]);
+    chars_per_line = w/char_width;
+    num_lines = txt.length()/chars_per_line + 1;
+    for(int i=0; i<num_lines; i++)
+    {
+        draw_text_line(x, y+(i*line_height), txt.substr(i*chars_per_line, chars_per_line), type, r, g, b);
+    }
+}
+
+/*
+ * Tiles and Maps
+ */
+
+/* Draw a tile such that its upper-left pixel is at (x, y) */
 void Display::draw_tile(int x, int y, Tile::TileImgId id)
 {
     SDL_Rect srcrect, dstrect;
@@ -42,6 +97,7 @@ void Display::draw_tile(int x, int y, Tile::TileImgId id)
     return;
 }
 
+/* Draws a rectangle of tiles from a map */
 void Display::draw_map(int x, int y, Map *m, int mx, int my, int mw, int mh)
 {
     fill_rect(x, y, mw*24, mh*24, 0, 0, 0);
@@ -57,50 +113,19 @@ void Display::draw_map(int x, int y, Map *m, int mx, int my, int mw, int mh)
     return;
 }
 
+/* Draws an entire map */
 void Display::draw_map(int x, int y, Map *m)
 {
     draw_map(x, y, m, 0, 0, m->getWidth(), m->getHeight());
     return;
 }
 
-/* Text drawing */
-void Display::draw_text_line(SDL_Rect *dst, std::string txt, FontType type, SDL_Color col)
-{
-    SDL_Surface *render = TTF_RenderText_Solid(font[type], txt.c_str(), col);
-    SDL_BlitSurface(render, NULL, gScreen, dst);
-    SDL_FreeSurface(render);
-    return;
-}
-
-void Display::draw_text_line(int x, int y, std::string txt, FontType type, int r, int g, int b)
-{
-    SDL_Color col;
-    SDL_Rect dst;
-
-    col.r = r;
-    col.g = g;
-    col.b = b;
-    dst.x = x;
-    dst.y = y;
-
-    draw_text_line(&dst, txt, type, col);
-}
-
-/* draw a text block, top-left corner at (x, y) with width w, with REALLY basic character wrapping */
-void Display::draw_text_block(int x, int y, int w, std::string txt, FontType type, int r, int g, int b)
-{
-    int char_width, line_height, chars_per_line, num_lines;
-    TTF_SizeText(font[type], "a", &char_width, NULL);
-    line_height = TTF_FontLineSkip(font[type]);
-    chars_per_line = w/char_width;
-    num_lines = txt.length()/chars_per_line + 1;
-    for(int i=0; i<num_lines; i++)
-    {
-        draw_text_line(x, y+(i*line_height), txt.substr(i*chars_per_line, chars_per_line), type, r, g, b);
-    }
-}
-
-/* Initialize graphics */
+/*
+ * Initializer:
+ * Initializes SDL and friends
+ * Creates window
+ * Loads images from files
+ */
 Display::Display(int width, int height)
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
--- Display.h
+++ Display.h
@@ -8,13 +8,11 @@ Contains the Display class, which creates the window and handles graphical drawi
 General argument order for Display items:
 target position
 target size
-source (image, text, etc.)
-source attributes (e.g. for text):
-    size
-    color
-
+source (describes the actual image, text, etc.)
+source attributes (e.g. text font, rectangle color)
 source position (for cropping)
 source size (for cropping)
+
 */
 
 #include <string>
@@ -36,6 +34,7 @@ class Display
 {
     SDL_Surface *gScreen;
     SDL_Surface *tilesheet;
+    SDL_Surface *spritesheet;
     TTF_Font *font[2];
 
 public:
@@ -51,15 +50,18 @@ public:
     };
 
     // methods
+    // primitives
+    void update();
     void putpixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
-    void draw_tile(int x, int y, Tile::TileImgId id);
     void fill_rect(int x, int y, int w, int h, int r, int g, int b);
-    void draw_map(int x, int y, Map *m);
-    void draw_map(int x, int y, Map *m, int mx, int my, int mw, int mh);
+    // text
     void draw_text_line(SDL_Rect *dst, std::string txt, FontType type, SDL_Color col);
     void draw_text_line(int x, int y, std::string txt, FontType type, int r, int g, int b);
     void draw_text_block(int x, int y, int w, std::string txt, FontType type, int r, int g, int b);
-    void update();
+    // tile & map
+    void draw_tile(int x, int y, Tile::TileImgId id);
+    void draw_map(int x, int y, Map *m);
+    void draw_map(int x, int y, Map *m, int mx, int my, int mw, int mh);
 };
 
 #endif
