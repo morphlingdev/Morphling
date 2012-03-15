7b0aa2fdd5615a245ab7f4b670387daca18d5729
Jim Zhang <Hyriodula@gmail.com>
2012-03-14 21:22:02 -0400

    makewin.sh now statically links libstdc++. Also, it does icons too. New Morphling.exe compiled using the updated makewin.sh. Not sure if the icon displays properly on Windows or not.

--- makewin.sh
+++ makewin.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-i486-mingw32-g++ -o Morphling.exe Morphling.cpp Display.cpp Ability.cpp Map.cpp Tile.cpp Entity.cpp MessageLog.cpp Item.cpp `/usr/i486-mingw32/bin/sdl-config --cflags --libs` -lSDL_ttf -lSDL_image -static-libgcc
+i486-mingw32-windres Morphling.rc -O coff -o Morphling.res
+i486-mingw32-g++ -o Morphling.exe Morphling.cpp Display.cpp Ability.cpp Map.cpp Tile.cpp Entity.cpp MessageLog.cpp Item.cpp Morphling.res `/usr/i486-mingw32/bin/sdl-config --cflags --libs` -lSDL_ttf -lSDL_image -static-libgcc -static-libstdc++
